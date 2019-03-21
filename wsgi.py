97181a4abe076e18ebfc4ab91a21ad8750ddb621
Edward Jose <ejose@example.com>
2019-03-21 14:47:11 +0800

    added flaskenv

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from flaskr import create_app, db
+# from app.models import User, Post, Message, Notification, Task
+
+app = create_app()
+# cli.register(app)
+
+
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
+#             'Notification': Notification, 'Task': Task}
